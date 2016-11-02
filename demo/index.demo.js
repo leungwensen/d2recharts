bb809fd39f1d1eafe0b511f8189b2819ea7f2691
wensen.lws <wensen.lws@alibaba-inc.com>
2016-11-02 18:04:02 +0800

    killer feature!

--- demo/index.demo.js
+++ demo/index.demo.js
@@ -7,49 +7,21 @@
 const d2recharts = require('d2recharts');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const D2Recharts = d2recharts.D2Recharts;
+require('./index.less');
 
-const option = {
-  color: ['#3398DB'],
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'shadow',
-    },
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true,
-  },
-  xAxis: [
-    {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      axisTick: {
-        alignWithLabel: true,
-      },
-    }
-  ],
-  yAxis: [
-    {
-      type: 'value',
-    }
-  ],
-  series: [
-    {
-      name: '直接访问',
-      type: 'bar',
-      barWidth: '60%',
-      data: [10, 52, 200, 334, 390, 330, 220],
-    }
-  ],
-};
+const D2Pie = d2recharts.D2Pie;
+
+const data = [
+  {genre: 'Sports', sold: 275},
+  {genre: 'Strategy', sold: 115},
+  {genre: 'Action', sold: 120},
+  {genre: 'Shooter', sold: 350},
+  {genre: 'Other', sold: 150}
+];
 
 ReactDOM.render(
   (
-    <D2Recharts option={option}/>
+    <D2Pie data={data}/>
   ),
   document.getElementById('canvas')
 );

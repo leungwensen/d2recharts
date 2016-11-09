f2eae362c0b72b21bcd3934beee86f12b8860bd2
leungwensen <leungwensen@gmail.com>
2016-11-09 15:36:30 +0800

    D2Text, D2Table, etc.

--- demo/index.demo.js
+++ demo/index.demo.js
@@ -21,14 +21,8 @@ const data = [
 $.get('./data/csv/population-china.csv', (csv) => {
   ReactDOM.render(
     (
-      /*<d2recharts.D2Bar data={csv}/>*/
-      /*<d2recharts.D2Line data={csv}/>*/
-      <d2recharts.D2Pie data={csv} dimension="统计时间" measures={["年末人口"]}/>
-      /*<d2recharts.D2Radar data={csv}/>*/
-      /*<d2recharts.D2Gauge data={csv} rowIndex="2" measures={['price']}/>*/
+      <d2recharts.D2Text data={csv}/>
     ),
     document.getElementById('canvas')
   );
 }, 'text');
-
-

afbc8a0d4877d0e410952f6f546ab2e53d92fee4
leungwensen <leungwensen@gmail.com>
2016-12-12 14:36:49 +0800

    #1 性能问题, etc.

--- demo/index.demo.js
+++ demo/index.demo.js
@@ -1,32 +1,38 @@
-'use strict';
+"use strict";
 /**
  * index.demo module
  * @module index.demo
  * @see module:index
  */
-const d2recharts = require('d2recharts');
-const React = require('react');
-const $ = require('jquery');
-const ReactDOM = require('react-dom');
-require('./index.less');
+const d2recharts = require("d2recharts");
+const React = require("react");
+const $ = require("jquery");
+const ReactDOM = require("react-dom");
+require("./index.less");
 
 const data = [
-  {genre: 'Sports', sold: 275, price: 30},
-  {genre: 'Strategy', sold: 115, price: 100},
-  {genre: 'Action', sold: 120, price: 130},
-  {genre: 'Shooter', sold: 350, price: 120},
-  {genre: 'Other', sold: 150, price: 106}
+  {genre: "Sports", sold: 275, price: 30},
+  {genre: "Strategy", sold: 115, price: 100},
+  {genre: "Action", sold: 120, price: 130},
+  {genre: "Shooter", sold: 350, price: 120},
+  {genre: "Other", sold: 150, price: 106}
 ];
 
-$.get('./data/csv/population-china.csv', (csv) => {
-  ReactDOM.render(
-    (
-      <div>
-        <d2recharts.Funnel data={csv}/>
-        <d2recharts.Smart data={csv}/>
-        <d2recharts.Indicator data={csv} height="auto"/>
-      </div>
-    ),
-    document.getElementById('canvas')
-  );
-}, 'text');
+const before = Date.now();
+ReactDOM.render(
+  (
+    <div>
+      <d2recharts.Bar data={data}/>
+      <d2recharts.Funnel data={data}/>
+      <d2recharts.Gauge data={data}/>
+      <d2recharts.Indicator data={data} height="auto"/>
+      <d2recharts.Line data={data}/>
+      <d2recharts.Pie data={data}/>
+      <d2recharts.Radar data={data}/>
+      <d2recharts.Scatter data={data}/>
+      <d2recharts.Smart data={data}/>
+    </div>
+  ),
+  document.getElementById("canvas")
+);
+console.log(Date.now() - before);

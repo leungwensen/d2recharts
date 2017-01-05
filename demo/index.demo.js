0c21f4b523a5efd6d6beac5f2e8dc96ad4179a75
leungwensen <leungwensen@gmail.com>
2017-01-05 14:18:45 +0800

    #8 invalid data

--- demo/index.demo.js
+++ demo/index.demo.js
@@ -6,31 +6,41 @@
  */
 const d2recharts = require("d2recharts");
 const React = require("react");
-const $ = require("jquery");
+// const $ = require("jquery");
 const ReactDOM = require("react-dom");
 require("./index.less");
 
-const data = [
-  {genre: "Sports", sold: 275, price: 30},
-  {genre: "Strategy", sold: 115, price: 100},
-  {genre: "Action", sold: 120, price: 130},
-  {genre: "Shooter", sold: 350, price: 120},
-  {genre: "Other", sold: 150, price: 106}
-];
+const data = {
+  data: [
+    {genre: "Sports", sold: 275, price: 30},
+    {genre: "Strategy", sold: 115, price: 100},
+    {genre: "Action", sold: 120, price: 130},
+    {genre: "Shooter", sold: 350, price: 120},
+    {genre: "Other", sold: 150, price: 106}
+  ],
+  schema: [
+    {name: "genre", comments: "分类"},
+    {name: "sold", comments: "销量"},
+    // {name: "price", comments: "价格"},
+    {name: "xxx", comments: "yyy"},
+  ]
+};
 
 const before = Date.now();
+const padding = 'none';
+
 ReactDOM.render(
   (
     <div>
-      <d2recharts.Bar data={data}/>
-      <d2recharts.Line data={data}/>
-      <d2recharts.Scatter data={data}/>
-      <d2recharts.Funnel data={data}/>
-      <d2recharts.Gauge data={data}/>
-      <d2recharts.Indicator data={data} height="auto"/>
-      <d2recharts.Pie data={data}/>
-      <d2recharts.Radar data={data}/>
-      <d2recharts.Smart data={data}/>
+      <d2recharts.Bar padding={padding} data={data}/>
+      <d2recharts.Line padding={padding} data={data}/>
+      <d2recharts.Scatter padding={padding} data={data}/>
+      <d2recharts.Funnel padding={padding} data={data}/>
+      <d2recharts.Gauge padding={padding} data={data}/>
+      <d2recharts.Indicator padding={padding} data={data} height="auto"/>
+      <d2recharts.Pie padding={padding} data={data}/>
+      <d2recharts.Radar padding={padding} data={data}/>
+      <d2recharts.Smart padding={padding} data={data}/>
     </div>
   ),
   document.getElementById("canvas")
